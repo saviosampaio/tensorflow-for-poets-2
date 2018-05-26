ca47b3faa0205ac28ba89884eaac46bebe1adc67
saviosampaio <saviosampaio@gmail.com>
2018-05-26 19:16:39 -0300

    Update clear_cache.py

--- scripts/clear_cache.py
+++ scripts/clear_cache.py
@@ -193,13 +193,13 @@ def create_image_lists(image_dir, testing_percentage, validation_percentage):
       
       # primeiro criterio de divisao sera o nome do arquivo, se contem train_ val_ test_ na frente
       # segundo criterio sera randomico com base no percentual
-      if file_name.index("_train_") != -1:
+      if file_name.find("_train_") != -1:
           tf.logging.info("_TRAIN_ ENCONTRADO '" + file_name + "'")
           training_images.append(base_name)
-      elif file_name.index("_val_") != -1:
+      elif file_name.find("_val_") != -1:
           tf.logging.info("_VAL_ ENCONTRADO '" + file_name + "'")
           validadion_images.append(base_name)
-      elif file_name.index("_test_") != -1:
+      elif file_name.find("_test_") != -1:
           tf.logging.info("_TEST_ ENCONTRADO '" + file_name + "'")
           testing_images.append(base_name)
       else:
