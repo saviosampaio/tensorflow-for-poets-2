b2268fead404a63c0c2c3e0c4562a9e4d3d603f7
saviosampaio <saviosampaio@gmail.com>
2018-05-28 06:46:51 -0300

    Update clear_cache.py

--- scripts/clear_cache.py
+++ scripts/clear_cache.py
@@ -882,7 +882,8 @@ def create_model_info(architecture):
   architecture = architecture.lower()
   if architecture == 'inception_v3':
     # pylint: disable=line-too-long
-    data_url = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'
+    #data_url = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'
+    data_url = 'http://download.tensorflow.org/models/image/imagenet/inception-v3-2016-03-01.tar.gz'
     # pylint: enable=line-too-long
     bottleneck_tensor_name = 'pool_3/_reshape:0'
     bottleneck_tensor_size = 2048
@@ -893,7 +894,7 @@ def create_model_info(architecture):
     model_file_name = 'classify_image_graph_def.pb'
     input_mean = 128
     input_std = 128
-  elif architecture == 'inception_v':
+  elif architecture == 'inception_v4':
     # pylint: disable=line-too-long
     #data_url = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'
     #data_url = 'http://download.tensorflow.org/models/image/imagenet/inception-v3-2016-03-01.tar.gz'
@@ -910,8 +911,8 @@ def create_model_info(architecture):
     input_std = 128
   elif architecture == 'inception_v3b':
     # pylint: disable=line-too-long
-    #data_url = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'
-    data_url = 'http://download.tensorflow.org/models/image/imagenet/inception-v3-2016-03-01.tar.gz'
+    data_url = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'
+    #data_url = 'http://download.tensorflow.org/models/image/imagenet/inception-v3-2016-03-01.tar.gz'
     # pylint: enable=line-too-long
     bottleneck_tensor_name = 'pool_3/_reshape:0'
     bottleneck_tensor_size = 2048
